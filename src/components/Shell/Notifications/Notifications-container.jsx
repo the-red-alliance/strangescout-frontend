8b5838673750c3531bb4c903b0685b1149e0936d
Noah Piraino <penguinsnail@onebuttonmouse.com>
2019-12-11 19:33:37 -0500

    display invite creation errors

--- src/components/Shell/Notifications/Notifications-container.jsx
+++ src/components/Shell/Notifications/Notifications-container.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { clearUserError } from '../../../store/user/actions';
+import { clearInviteError } from '../../../store/invite/actions';
 
 // import shell content
 import { Notifications as Notification } from './Notifications-content.jsx';
@@ -9,20 +10,18 @@ import { Notifications as Notification } from './Notifications-content.jsx';
 // map user from store to prop
 function mapStateToProps(state) {
 	return {
-		user: state.user
+		user: state.user,
+		invite: state.invite,
 	};
 };
 
 function NotificationsContainer(props) {
-	const { user } = props;
-
-	function closeUserError() {
-		props.dispatch(clearUserError());
-	};
+	const { user, invite } = props;
 
 	return (
 		<React.Fragment>
-			<Notification open={Boolean(user.error)} variant="error" message={user.error} handleClose={closeUserError} />
+			<Notification open={Boolean(user.error)} variant="error" message={user.error} handleClose={() => props.dispatch(clearUserError())} />
+			<Notification open={Boolean(invite.error)} variant="error" message={invite.error} handleClose={() => props.dispatch(clearInviteError())} />
 		</React.Fragment>
 	);
 };
