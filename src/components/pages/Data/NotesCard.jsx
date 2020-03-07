8742832b97829494b51b3631b7ec45f6d92a89d1
Noah Piraino <penguinsnail@onebuttonmouse.com>
2020-03-07 13:48:43 -0500

    debug lines

--- src/components/pages/Data/NotesCard.jsx
+++ src/components/pages/Data/NotesCard.jsx
@@ -25,6 +25,8 @@ export function NotesCard(props) {
 	const { runs } = props;
 	const classes = useStyles();
 
+	console.log(runs, runs.map(run => run.notes));
+
 	if (runs.map(run => run.notes).filter(el => el).length < 1) return <React.Fragment />;
 
 	return (
